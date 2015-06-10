b33a8e89008ec3cf68c7591010a9b2d9a63057a5
Marcelo Mingatos <marcelo.m.toledo@gmail.com>
2015-06-10 01:03:07 -0300

    Cleanup

--- Makefile
+++ Makefile
@@ -15,7 +15,42 @@ server: build
 client: build
 	java -classpath src \
 	-Djava.rmi.server.hostname=192.168.1.156 \
+	MovieRentalClient $(ARGS)
+
+test1: build
+	java -classpath src \
+	-Djava.rmi.server.hostname=192.168.1.156 \
 	MovieRentalClient listTitlesAndYears
+
+test2: build
+	java -classpath src \
+	-Djava.rmi.server.hostname=192.168.1.156 \
+	MovieRentalClient listByGenre 0
+
+test3: build
+	java -classpath src \
+	-Djava.rmi.server.hostname=192.168.1.156 \
+	MovieRentalClient synopysis 1
+
+test4: build
+	java -classpath src \
+	-Djava.rmi.server.hostname=192.168.1.156 \
+	MovieRentalClient info 5
+
+test5: build
+	java -classpath src \
+	-Djava.rmi.server.hostname=192.168.1.156 \
+	MovieRentalClient list
+
+test6: build
+	java -classpath src \
+	-Djava.rmi.server.hostname=192.168.1.156 \
+	MovieRentalClient update 4 15
+
+test7: build
+	java -classpath src \
+	-Djava.rmi.server.hostname=192.168.1.156 \
+	MovieRentalClient quantity 3
 
 clean:
 	rm -f src/*.class
--- src/MovieRentalClient.java
+++ src/MovieRentalClient.java
@@ -9,48 +9,62 @@ public class MovieRentalClient {
         
         try {
             
-            MovieRentalInterface rentalStub  = (MovieRentalInterface) Naming.lookup("//" +
-                "192.168.1.156" + 
-                "/MovieRental");
-            long start, end, diff;
             Response response = new Response("", 1);
             Boolean timeOnly = true;
+                
 
             CommandParser parser = new CommandParser();
             CommandWrapper cmd = parser.parseCommand(args);
-            start = System.nanoTime();
 
-            switch (cmd.command) {
-                case LIST_ALL:
-                response = rentalStub.getAllMovies();
-                break;
-                case LIST_ALL_TITLES_AND_YEARS:
-                response = rentalStub.getAllNamesAndYears();
-                // System.err.println(response.toString());
-                break;
-                case LIST_ALL_BY_GENRE:
-                response = rentalStub.getAllTitlesAndYearByGenre(cmd.identifier);
-                break;
-                case SYNOPISIS_BY_ID:
-                response = rentalStub.getSynopsisById(cmd.identifier);
-                break;
-                case INFO_BY_ID:
-                response = rentalStub.getInfoById(cmd.identifier);
-                break;
-                case QUANTITY_BY_ID:
-                response = rentalStub.getQuantityOfMovie(cmd.identifier);
-                break;
-                case UPDATE_QUANTITY:
-                    response = rentalStub.setQuantityOfMovie(cmd.identifier, cmd.quantity);
-                break;
-            }
-            end = System.nanoTime();
-            diff = end-start;
+            long start, end, diff;
 
-            if (timeOnly) {
-                System.err.println(response.getData() + "-" + diff + "-" + response.getTime());
-            } else {
-                System.err.println(diff);
+            int count = 1;
+
+            while (count > 0) {
+                
+                start = System.nanoTime();
+
+                MovieRentalInterface rentalStub  = (MovieRentalInterface) Naming.lookup("//" +
+                    "192.168.1.156" + 
+                    "/MovieRental");
+                
+                switch (cmd.command) {
+                    case LIST_ALL:
+                    response = rentalStub.getAllMovies();
+                    break;
+                    case LIST_ALL_TITLES_AND_YEARS:
+                    response = rentalStub.getAllNamesAndYears();
+                    // System.err.println(response.toString());
+                    break;
+                    case LIST_ALL_BY_GENRE:
+                    response = rentalStub.getAllTitlesAndYearByGenre(cmd.identifier);
+                    break;
+                    case SYNOPISIS_BY_ID:
+                    response = rentalStub.getSynopsisById(cmd.identifier);
+                    break;
+                    case INFO_BY_ID:
+                    response = rentalStub.getInfoById(cmd.identifier);
+                    break;
+                    case QUANTITY_BY_ID:
+                    response = rentalStub.getQuantityOfMovie(cmd.identifier);
+                    break;
+                    case UPDATE_QUANTITY:
+                        response = rentalStub.setQuantityOfMovie(cmd.identifier, cmd.quantity);
+                    break;
+                }
+                end = System.nanoTime();
+                diff = end-start;
+
+                long readTime = response.getTime()/1000;
+                long comnTime = diff/1000;
+
+                if (timeOnly) {
+                    System.out.printf("%d,%d\n", readTime, comnTime);
+                } else {
+                    System.err.println(response.toString()+"|"+comnTime);
+                }
+
+                count = count - 1;
             }
 
         }
